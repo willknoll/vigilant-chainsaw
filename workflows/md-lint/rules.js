9ede5e647b7f31aec88a0242deedd65b49b963e1
Will Knoll <v-wiknol@microsoft.com>
2020-04-30 15:09:36 -0700

    Testing rule

--- workflows/md-lint/rules.js
+++ workflows/md-lint/rules.js
@@ -2,13 +2,22 @@
 
 "use strict";
 
-const anyBlockquote = require("./any-blockquote");
-module.exports.anyBlockquote = anyBlockquote;
-
-const noSafeLinks = require("./no-safelinks");
-module.exports.noSafeLinks = noSafeLinks;
-
-module.exports.all = [
-  anyBlockquote,
-  noSafeLinks
-];
+module.exports = {
+  "names": [ "no-safelinks" ],
+  "description": "No SafeLinks allowed for URLs",
+  "tags": [ "links" ],
+  "function": function safelinkRule(params, onError) {
+	params.tokens.filter(function filterToken(token) {
+      return token.type === "link_open";
+    }).forEach(function forToken(linktoken) {
+		var href = token.attrs["href"];
+		if (/safelinks/i.test(href)) {
+			onError({
+				"lineNumber": linktoken.lineNumber,
+				"detail": "Link uses SafeLink"",
+				"context": linktoken.line.substr(0, 7)
+			});
+		}
+	}
+  }
+};
