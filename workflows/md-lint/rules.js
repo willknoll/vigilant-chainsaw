5089f4d2f96038c1c34e971c862fb064424e96fe
Will Knoll <v-wiknol@microsoft.com>
2020-04-30 16:15:48 -0700

    trying different rule path

--- workflows/md-lint/rules.js
+++ workflows/md-lint/rules.js
@@ -2,10 +2,10 @@
 
 "use strict";
 
-const anyBlockquote = require("./any-blockquote");
+const anyBlockquote = require("any-blockquote");
 module.exports.anyBlockquote = anyBlockquote;
 
-const noSafeLinks = require("./no-safelinks");
+const noSafeLinks = require("no-safelinks");
 module.exports.noSafeLinks = noSafeLinks;
 
 module.exports.all = [
