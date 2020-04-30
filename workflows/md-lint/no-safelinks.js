6c2011e870d5c6825af22bb4d3a9cf8f7377622d
Will Knoll <v-wiknol@microsoft.com>
2020-04-30 15:26:46 -0700

    testing

--- workflows/md-lint/no-safelinks.js
+++ workflows/md-lint/no-safelinks.js
@@ -6,6 +6,7 @@ module.exports = {
   "names": [ "no-safelinks" ],
   "description": "No SafeLinks allowed for URLs",
   "tags": [ "links" ],
+  "information": new URL("https://example.com/rules/no-safelinks"),
   "function": function safelinkRule(params, onError) {
 	params.tokens.filter(function filterToken(token) {
       return token.type === "link_open";
