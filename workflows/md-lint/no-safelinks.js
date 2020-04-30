0e9e9f5781d645f2157e3e3831d9b463cd6ff289
Will Knoll <v-wiknol@microsoft.com>
2020-04-30 16:08:29 -0700

    Trying multiple custom rules

--- workflows/md-lint/no-safelinks.js
+++ workflows/md-lint/no-safelinks.js
@@ -5,21 +5,21 @@
 module.exports = {
   "names": [ "no-safelinks" ],
   "description": "No SafeLinks allowed for URLs",
-  "tags": [ "links" ],
   "information": new URL("https://example.com/rules/no-safelinks"),
-  "function": function safelinkRule(params, onError) {
+  "tags": [ "links" ],
+  "function": function rule(params, onError) {
 	params.tokens.filter(function filterToken(token) {
-      return token.type === "link_open";
+      return token.type === "link_close";
     }).forEach(function forToken(linktoken) {
 		var href = token.attrs["href"];
 		if (/safelinks/i.test(href)) {
 			onError({
 				"lineNumber": linktoken.lineNumber,
-				"detail": "Link uses SafeLink"",
+				"detail": "Link uses SafeLink",
 				"context": linktoken.line.substr(0, 7)
 			});
 		}
-	}
+	});
   }
 };
 	  
--- workflows/md-lint/rules.js
+++ workflows/md-lint/rules.js
@@ -2,23 +2,13 @@
 
 "use strict";
 
-module.exports = {
-  "names": [ "no-safelinks" ],
-  "description": "No SafeLinks allowed for URLs",
-  "information": new URL("https://example.com/rules/no-safelinks"),
-  "tags": [ "links" ],
-  "function": function rule(params, onError) {
-	params.tokens.filter(function filterToken(token) {
-      return token.type === "link_close";
-    }).forEach(function forToken(linktoken) {
-		var href = token.attrs["href"];
-		if (/safelinks/i.test(href)) {
-			onError({
-				"lineNumber": linktoken.lineNumber,
-				"detail": "Link uses SafeLink",
-				"context": linktoken.line.substr(0, 7)
-			});
-		}
-	});
-  }
-};
+const anyBlockquote = require("./any-blockquote");
+module.exports.anyBlockquote = anyBlockquote;
+
+const noSafeLinks = require("./no-safelinks");
+module.exports.noSafeLinks = noSafeLinks;
+
+module.exports.all = [
+  anyBlockquote,
+  noSafeLinks
+];
