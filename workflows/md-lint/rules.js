fc26fceb44a02082c7385475bd79f11b3e7c9d4b
Will Knoll <v-wiknol@microsoft.com>
2020-04-30 16:34:05 -0700

    adjusting custom rule

--- workflows/md-lint/rules.js
+++ workflows/md-lint/rules.js
@@ -2,12 +2,17 @@
 
 "use strict";
 
-const anyBlockquote = require("any-blockquote");
+const anyBlockquote = require(".\any-blockquote");
 module.exports.anyBlockquote = anyBlockquote;
 
-const noSafeLinks = require("no-safelinks");
+const noSafeLinks = require(".\no-safelinks");
 module.exports.noSafeLinks = noSafeLinks;
 
+const simpleRule = require(".\simple-rule");
+module.exports.simpleRule = simpleRule;
+
 module.exports.all = [
-  anyBlockquote
+  anyBlockquote,
+  noSafeLinks,
+  simpleRule
 ];
