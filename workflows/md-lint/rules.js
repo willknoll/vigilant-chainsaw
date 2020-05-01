084d54e654e7429902f50b1d41f6c0790bb21b5c
Will Knoll <v-wiknol@microsoft.com>
2020-04-30 18:24:06 -0700

    Revert back to a single rule

--- workflows/md-lint/rules.js
+++ workflows/md-lint/rules.js
@@ -2,14 +2,39 @@
 
 "use strict";
 
-const anyBlockquote = require("./any-blockquote");
-module.exports.anyBlockquote = anyBlockquote;
+module.exports = {
+  "names": [ "no-safelinks" ],
+  "description": "No SafeLinks allowed for URLs",
+  "information": new URL("https://example.com/rules/no-safelinks"),
+  "tags": [ "links" ],
+  "function": function rule(params, onError) {
+	params.tokens.filter(function filterToken(token) {
+      return token.type === "link_close";
+    }).forEach(function forToken(linktoken) {
+		var href = token.attrs["href"];
+		if (/safelinks/i.test(href)) {
+			onError({
+				"lineNumber": linktoken.lineNumber,
+				"detail": "Link uses SafeLink",
+				"context": linktoken.line.substr(0, 7)
+			});
+		}
+	});
+  }
+};
 
-const noSafeLinks = require("./no-safelinks");
-module.exports.noSafeLinks = noSafeLinks;
+// @ts-check
 
-const simpleRule = require("./simple-rule");
-module.exports.simpleRule = simpleRule;
+//"use strict";
+
+//const anyBlockquote = require("./any-blockquote");
+//module.exports.anyBlockquote = anyBlockquote;
+
+//const noSafeLinks = require("./no-safelinks");
+//module.exports.noSafeLinks = noSafeLinks;
+
+//const simpleRule = require("./simple-rule");
+//module.exports.simpleRule = simpleRule;
 
 //module.exports.all = [
 //  anyBlockquote,
