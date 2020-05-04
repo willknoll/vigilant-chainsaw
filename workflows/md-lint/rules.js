a9413ecdf120c0b60e33e3d7d8933e48c719599b
Will Knoll <v-wiknol@microsoft.com>
2020-05-04 11:55:25 -0700

    Remove log spamming from rules

--- workflows/md-lint/rules.js
+++ workflows/md-lint/rules.js
@@ -2,57 +2,6 @@
 
 //"use strict";
 
-/* module.exports = {
-  "names": [ "sample-rule" ],
-  "description": "Sample rule",
-  "tags": [ "sample" ],
-  "function": function rule(params, onError) {
-    params.tokens.forEach((token) => {
-		console.log(token.type);
-      if (token.type === "hr") {
-        onError({
-          "lineNumber": token.lineNumber,
-          "detail": "Sample error for hr"
-        });
-      }
-    });
-  }
-}; */
-
-/* const testSafeLink = "/safelinks\.protection/i";
-
-module.exports = {
-  "names": [ "no-safelinks" ],
-  "description": "No SafeLinks allowed for URLs",
-  "information": new URL("https://example.com/rules/no-safelinks"),
-  "tags": [ "links" ],
-  "function": function rule(params, onError) {
-    params.tokens.filter(function filterToken(token) {
-      return token.type === "link_close";
-    }).forEach(function forToken(linktoken) {
-        let href = linktoken.attrGet["href"];
-        if (href)
-        {
-          let isSafeLink = testSafeLink.test(href)
-
-          //var href = 
-          //if (/safelinks/i.test(href))
-		  if (isSafeLink)
-          {
-            onError({
-              "lineNumber": linktoken.lineNumber,
-              "detail": "Link uses SafeLink",
-              "context": linktoken.line.substr(0, 7)
-            });
-           //}
-          }
-	    }
-    });
-  }
-}; */
-
-//const testExternal = /^(?:https?\:)?\/\//;
-//const testValidRelative = /^(?:\.\.?\/)/;
 const testSafeLink = new RegExp("safelinks\.protection");
 module.exports = {
     names: ["MD100", "any-safelink-url"],
@@ -63,9 +12,9 @@ module.exports = {
 			let links = token.children.filter(t => t.type === "link_open");
 			for (let alink of links)
 			{
-				console.log('New link found:');
+				//console.log('New link found:');
 				let href = alink.attrGet('href');
-				console.log('  ' + href);
+				//console.log('  ' + href);
 				if (testSafeLink.test(href.toLowerCase()))
 				{
 					let index = alink.line.indexOf(href);
@@ -78,22 +27,8 @@ module.exports = {
 				}
 				else
 				{
-					console.log('  Not a SafeLink')
+					//console.log('  Not a SafeLink')
 				}
-				//if (child.type === "link_open")
-				//{
-					//console.log('New link child: ' + child.type);
-					//console.log('  Content: ' + child.content);
-					//console.log('  Info: ' + child.info);
-					//console.log('  Markup :' + child.markup);
-					//console.log('  Tag :' + child.tag);
-					//console.log('  Attributes:')
-					//for (let attribute of token.attrs)
-					//{
-						//let href = child.attrGet("href");
-						//console.log('    ' + href);
-						//console.log('    ' + testSafeLink.test(href.toLowerCase()));
-					//}
 			}
         });
 	}
